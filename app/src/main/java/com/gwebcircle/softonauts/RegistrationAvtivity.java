36940ce88b26b8817dbfb28f28087878d8f7ddb5
Disha-Mohite <disha.mohite56@gmail.com>
2022-03-11 12:15:24 +0530

    first commit

---
+++ app/src/main/java/com/gwebcircle/softonauts/RegistrationAvtivity.java
@@ -0,0 +1,14 @@
+package com.gwebcircle.softonauts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_avtivity);
+    }
+}
